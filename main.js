05c8b349b888f746a6cc0a025751ab5559d34989
CoDan123 <let2350@gmail.com>
2022-09-10 15:49:01 -0600

    products array

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
 const wrapper = document.querySelector(".slider-wrapper");
 const menuItems = document.querySelectorAll(".menu-item");
 
+const products = [
+    {
+        id: 1, 
+        title:"Air force", 
+        price: 119, 
+        colors: [
+            {
+                code:"black", 
+                img:"./"
+            },
+            {
+                code:"darkblue",
+                img:"./"
+            }
+        ]
+    },
+    {
+        id: 2, 
+        title:"Air Jordan", 
+        price: 149, 
+        colors: [
+            {
+                code:"black", 
+                img:"./"
+            },
+            {
+                code:"darkblue",
+                img:"./"
+            }
+        ]
+    }
+];
+
 menuItems.forEach((item, index) => {
     item.addEventListener("click", ()=>{
         wrapper.style.transform = `translateX(${-100 * index}vw)`
     });
 });
+
