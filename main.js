ea24977841c81bc078aaf0954b53be0e830b9991
CoDan123 <let2350@gmail.com>
2022-09-13 20:01:51 -0600

    js module open/close and gallery section added

--- main.js
+++ main.js
@@ -126,3 +126,15 @@ currentProductSizes.forEach((size, index) => {
     })
 })
 
+const productButton = document.querySelector(".product-button");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", ()=>{
+    payment.style.display = "flex"
+});
+
+close.addEventListener("click", ()=>{
+    payment.style.display = "none"
+});
+
